195e2ce421912928b3d143975c34c7cd84d0e584
hyuna <kiipo0623@gmail.com>
2021-01-01 07:17:53 +0900

    manygallery

--- app/src/main/java/com/example/madcamp2020/Fragment2.java
+++ app/src/main/java/com/example/madcamp2020/Fragment2.java
@@ -1,7 +1,8 @@
-package com.example.madcamp2020;
+    package com.example.madcamp2020;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -18,7 +19,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -30,20 +33,16 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.zip.Inflater;
 
-public class Fragment2 extends Fragment {
+    public class Fragment2 extends Fragment {
 
-    private static final int PICK_FROM_ALBUM = 1;
-    private static final int PICK_FROM_CAMERA = 2;
-    private static final String TAG = "blackJin";
-    private File tempFile;
-
-    private Boolean isPermission = true;
+    private MyCursorAdapter adapter;
+    private Intent intent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         tedPermission();
     }
 
@@ -52,7 +51,7 @@ public class Fragment2 extends Fragment {
         PermissionListener permissionListener = new PermissionListener() {
             @Override
             public void onPermissionGranted() {
-                //권한 요청 성공
+                //권한 요청 성공 : 여기 코드 작성
             }
 
             @Override
@@ -69,149 +68,30 @@ public class Fragment2 extends Fragment {
                 .check();
     }
 
-    private void goToAlbum(){
-        Log.e(TAG, "ALBUM GO");
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, PICK_FROM_ALBUM);
-
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode != Activity.RESULT_OK) {
-
-            Toast.makeText(getContext(), "취소 되었습니다.", Toast.LENGTH_SHORT).show();
-
-            if(tempFile != null) {
-                if (tempFile.exists()) {
-                    if (tempFile.delete()) {
-                        Log.e(TAG, tempFile.getAbsolutePath() + " 삭제 성공");
-                        tempFile = null;
-                    }
-                }
-            }
-
-            return;
-        }
-
-        if(requestCode == PICK_FROM_ALBUM){
-            Uri photoUri = data.getData();
-            Cursor cursor = null;
-
-            try{
-                String[] proj = {MediaStore.Images.Media.DATA};
-                assert photoUri != null;
-                cursor = getActivity().getContentResolver().query(photoUri, proj, null, null, null);
-
-                assert cursor != null;
-                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-
-                cursor.moveToFirst();
-
-                tempFile = new File(cursor.getString(column_index));
-            }finally {
-                if(cursor!=null){
-                    cursor.close();
-                }
-            }
-            setImage();
-        }else if(requestCode == PICK_FROM_CAMERA){
-            setImage();
-        }
-    }
-
-    private void setImage(){
-        ImageView imageView = (ImageView)getView().findViewById(R.id.imageView);
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
-
-        imageView.setImageBitmap(originalBm);
-    }
-
-
-    private void takePhoto(){
-        Log.e(TAG,  "PHOTO GO");
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        try{
-            tempFile = createImageFile();
-        }catch(IOException e){
-            Toast.makeText(getContext(), "이미지 처리 오류! 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
-            getActivity().getSupportFragmentManager().popBackStack();
-            e.printStackTrace();
-        }
-
-        if (tempFile != null) {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-
-                Uri photoUri = FileProvider.getUriForFile(getContext().getApplicationContext(),
-                        "com.example.madcamp2020.provider", tempFile);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                startActivityForResult(intent, PICK_FROM_CAMERA);
-
-            } else {
-
-                Uri photoUri = Uri.fromFile(tempFile);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                startActivityForResult(intent, PICK_FROM_CAMERA);
-
-            }
-        }
-    }
-
-    private File createImageFile() throws IOException {
-
-        // 이미지 파일 이름 ( blackJin_{시간}_ )
-        String timeStamp = new SimpleDateFormat("HHmmss").format(new Date());
-        String imageFileName = "blackJin_" + timeStamp + "_";
-
-        // 이미지가 저장될 폴더 이름 ( blackJin )
-        File storageDir = new File(Environment.getExternalStorageDirectory() + "/blackJin/");
-        if (!storageDir.exists()) storageDir.mkdirs();
-
-        // 파일 생성
-        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
-        Log.d(TAG, "createImageFile : " + image.getAbsolutePath());
-
-        return image;
-    }
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_2, container, false);
+        GridView gridview = v.findViewById(R.id.photo_list);
+        Cursor cursor = getActivity().getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, MediaStore.Images.ImageColumns.DATE_TAKEN+" DESC");
 
-        Button btngallery = (Button)v.findViewById(R.id.btnGallery);
-        btngallery.setOnClickListener(new View.OnClickListener() {
+        adapter = new MyCursorAdapter(getActivity(), cursor);
+        gridview.setAdapter(adapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                // 권한 허용에 동의하지 않았을 경우 토스트를 띄웁니다.
-                if(isPermission) goToAlbum();
-                else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = (Cursor)parent.getAdapter().getItem(position);
+                String path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+                Toast.makeText(getActivity(), "사진경로 : "+path, Toast.LENGTH_SHORT).show();
+
+                intent = new Intent(getActivity(), DrawBigImage.class);
+                intent.putExtra("path", path);
+                startActivity(intent);
             }
         });
 
-        Button btncam = (Button)v.findViewById(R.id.btnCamera);
-        btncam.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 권한 허용에 동의하지 않았을 경우 토스트를 띄웁니다.
-                if(isPermission)  takePhoto();
-                else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
-            }
-        });
-
-        // Inflate the layout for this fragment
         return v;
-
-
     }
 
-}
+    }
